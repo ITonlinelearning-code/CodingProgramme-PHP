ea284386d2eb1e574d19826b34aedb622623c9c7
ITonlinelearning-code <167775962+ITonlinelearning-code@users.noreply.github.com>
2024-06-25 05:37:42 +0200

    Add files via upload

---
+++ PHP_01_01_Basics_of_PHP/index.php
@@ -0,0 +1,22 @@
+<?php
+// index.php
+
+// A simple PHP script that outputs the name and age of a user.
+
+/*
+  This script demonstrates basic PHP syntax, including variable declarations,
+  outputting data using echo, and commenting in PHP.
+*/
+
+// Declaring a string variable to store the user's name
+$userName = "John";
+
+// Declaring a number variable to store the user's age
+$userAge = 32;
+
+// Outputting the user's name and age using echo
+echo "Hello $userName, your age is $userAge!";
+
+// The following line of code is commented out and will not be executed
+// echo "This line will not be displayed in the browser.";
+?>
